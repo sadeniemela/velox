2705a64cefba222b6b780620be7f17146604e9b8
Sade <sademarian@gmail.com>
2026-06-09 12:28:38 +0200

    gsap config and lenis hook

---
+++ hooks/useLenis.ts
@@ -0,0 +1,27 @@
+"use client"
+
+import { useEffect } from "react"
+import Lenis from "lenis"
+import { ScrollTrigger } from "@/lib/gsap"
+
+export function useLenis() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    })
+
+    lenis.on("scroll", ScrollTrigger.update)
+
+    const raf = (time: number) => {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    return () => {
+      lenis.destroy()
+    }
+  }, [])
+}
